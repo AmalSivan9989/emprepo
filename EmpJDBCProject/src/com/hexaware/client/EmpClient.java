3055fa35eaf2524471334a510316fd910a57d1a9
AmalSivan9989 <amalsivan17532gmail.com>
2025-04-08 12:20:09 +0530

    added display method in emp client

--- EmpJDBCProject/src/com/hexaware/client/EmpClient.java
+++ EmpJDBCProject/src/com/hexaware/client/EmpClient.java
@@ -2,6 +2,11 @@ package com.hexaware.client;
 
 public class EmpClient {
     public static void main(String[] args) {
+        display();
         System.out.println("welcome");
+    }
+
+    public static void display(){
+        System.out.println("display");
     }
 }
